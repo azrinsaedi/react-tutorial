87caad5050e5f4f9cab430dc6cf108825f243a06
azrinsaedi <azrinsaedi@gmail.com>
2023-06-22 23:49:19 +0800

    react props practice

---
+++ react-props-practice/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import contacts from "../contacts";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+    </div>
+  );
+}
+
+export default App;
