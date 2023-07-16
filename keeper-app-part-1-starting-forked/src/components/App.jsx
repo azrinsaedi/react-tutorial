4cd4873693f3c4646aa713054357646b419a6f8a
azrin-t05 <azrin@t05.sg>
2023-07-17 00:13:22 +0800

    organize tutorial

---
+++ keeper-app-part-1-starting-forked/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./Footer";
+import Headers from "./Header";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Headers />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
